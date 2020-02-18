ee3012d644c65bcb84b1b0714fb39f523338adad
Willian Menezes <willian_menezes_santos@hotmail.com>
2020-02-17 21:15:32 -0300

    Criado projeto com bibliotecas de classes e contexto de banco de dados

---
+++ Repository/Contexts/Context.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Contexts
+{
+    public class Context : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public Context(DbContextOptions<Context> options, IConfiguration configuration) : base(options)
+        {
+            this._configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Default"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.HasPostgresExtension("uuid - ossp");
+        }
+    }
+}
